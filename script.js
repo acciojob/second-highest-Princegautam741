3381c506e1779d47b52b973d5691b3657d9a1c7d
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-11 17:59:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // The function to find the second-highest element
 function secondHighest(arr) {
     // Write your code here
+    // Check if the array is empty or has only one element
+    if (arr.length <= 1) {
+        return -Infinity;
+    }
+
+    // Remove duplicate elements and sort the array in descending order
+    const uniqueSortedArr = [...new Set(arr)].sort((a, b) => b - a);
+
+    // If all elements are the same, return -Infinity
+    if (uniqueSortedArr.length === 1) {
+        return -Infinity;
+    }
+
+    // Return the second-highest element
+    return uniqueSortedArr[1];
 }
 
 // Export the function for testing
