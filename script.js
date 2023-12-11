356c946d1f8763938c6fc232c6373c356ab944ec
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-11 17:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+// The function to find the second-highest element
+function secondHighest(arr) {
+    // Write your code here
+}
+
+// Export the function for testing
+module.exports = secondHighest;
